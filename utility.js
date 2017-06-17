d806e5d6727779b2bc5e08334fce31b345a77a2f
Ethan Stewart <ste11003@byui.edu>
2017-06-16 23:35:15 -0600

    Added server-side utility file

---
+++ utility.js
@@ -0,0 +1,32 @@
+exports.catchErrors = fn => {
+  return function(req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
+
+exports.devErr = (err, req, res, next) => {
+  err.stack = err.stack || '';
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
+  };
+  res.status(err.status || 500);
+  res.format({
+    // Based on the `Accept` http header
+    'text/html': () => {
+      res.render('error', errorDetails);
+    }, // Form Submit, Reload the page
+    'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
+  });
+};
+
+exports.prodErr = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+};
+
+exports.icon = name => require('fs').readFileSync(`./public/img/icons/${name}.svg`);
