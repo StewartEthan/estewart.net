74fc73629c3c8321baba299f7cc489fe145c11fd
Ethan Stewart <ste11003@byui.edu>
2017-09-30 15:04:58 -0600

    Initial work on Twitter feed api endpoint

--- controllers/routes.js
+++ controllers/routes.js
@@ -28,4 +28,27 @@ router.get('/about', (req,res) => {
   res.render('about');
 });
 
+// /api routes
+router.get('/api/ldsconf', (req,res) => {
+  // Set up config stuff
+  const twitterConfig = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+  };
+  const twitter = new (require('twitter'))(twitterConfig);
+  const searchParams = {
+    q: '#LDSconf',
+    count: 1
+  };
+
+  function tweetCb(err, tweets, response) {
+    res.json(tweets);
+  }
+
+  // Get the tweets
+  console.log('About to search tweets');
+  twitter.get('search/tweets', searchParams, tweetCb);
+});
+
 module.exports = router;
