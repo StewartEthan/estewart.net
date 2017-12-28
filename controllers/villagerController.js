58fb310056372666ca12cfcd33dee5b61f2e377a
Ethan Edward Stewart <ste11003@byui.edu>
2017-12-28 10:09:05 -0700

    Tweaked GET logic for villagers

--- controllers/villagerController.js
+++ controllers/villagerController.js
@@ -6,24 +6,19 @@ const Villager = mongoose.model('Villager');
 exports.createVillager = async (req,res) => {
   const villager = new Villager(req.body);
   await villager.save();
-  res.send(201)
+  res.send(201);
 };
 
 exports.getAllVillagers = async (req,res) => {
-  const villagers = await Villager.find({});
+  const villagers = await Villager.find({}).sort({ name: 1 });
   res.json(villagers);
 };
 
 exports.searchVillagers = async (req,res) => {
-  const { name } = req.params;
-  const query = {
-    $text: { $search: name }
-  };
-  const scoreOpt = {
-    score: { $meta: 'textScore' }
-  };
-  const villagers = await Villager
-    .find(query, scoreOpt)
-    .sort(scoreOpt);
+  const query = { $text: { $search: req.params.name } };
+  // Below line can be used for sorting if partial matching ever becomes a thing
+  // Use scoreOpt as second arg on `find` and as first arg on `sort` (chained)
+  // const scoreOpt = { score: { $meta: 'textScore' } };
+  const villagers = await Villager.find(query);
   res.json(villagers[0] || null);
 };
