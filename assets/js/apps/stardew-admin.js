c80754bad072f6e924d30bab2cfe5544820c2260
Ethan Edward Stewart <ste11003@byui.edu>
2017-12-27 18:21:57 -0700

    Basic styling and adjustment to stardew admin page

--- assets/js/apps/stardew-admin.js
+++ assets/js/apps/stardew-admin.js
@@ -2,8 +2,11 @@
 
 (function(document) {
   const createForm = document.querySelector('form.add-villager');
+  const villagerList = document.querySelector('.villager-list');
 
   createForm.addEventListener('submit', handleCreateVillager);
+
+  loadPage();
 
   function handleCreateVillager(e) {
     e.preventDefault();
@@ -19,5 +22,18 @@
     const headers = { 'Content-Type': 'application/json' };
 
     fetch('/stardew/villager', { method, body, headers });
+  }
+
+  async function loadPage() {
+    const villagers = await fetch('/stardew/villager/all').then(res => res.json());
+    villagers.forEach(villager => {
+      const div = document.createElement('div');
+      div.classList.add('villager');
+      div.innerHTML = `
+        <span>${villager.name}</span>
+        <button data-id="${villager._id}">Edit</button>
+      `;
+      villagerList.appendChild(div);
+    });
   }
 }(document));
