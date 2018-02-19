c3c30699708b5921c0bd510985c2eedf9643f703
Ethan Stewart <ethanstewart@familysearch.org>
2018-02-19 02:06:40 -0700

    Added tiny bit of basic game steps (requestAnimationFrame)

--- assets/js/apps/games/tower.js
+++ assets/js/apps/games/tower.js
@@ -1,3 +1,14 @@
 setTimeout(() => {
   document.dispatchEvent(new CustomEvent('es-game-loaded'));
 }, 2000);
+
+(function() {
+  // const canvas = document.querySelector('#game-el');
+  // const ctx = canvas.getContext('2d');
+
+  // function main(time) {
+  //   requestAnimationFrame(main);
+  // }
+
+  // requestAnimationFrame(main);
+}());
